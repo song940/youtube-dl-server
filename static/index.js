2b71f65e60a16c90fca858b0624aee28722eabe2
Lsong <song940@gmail.com>
2020-12-21 20:18:03 +0800

    first commit

---
+++ static/index.js
@@ -0,0 +1,11 @@
+import { ready } from 'https://lsong.org/scripts/dom.js';
+import { requestForm } from 'https://lsong.org/scripts/form.js';
+
+ready(() => {
+  requestForm('#form', (err, res) => {
+    console.log(err, res);
+    const {  title, url } = res;
+    const link = createLink(title, url);
+    document.body.appendChild(link);
+  });
+});
